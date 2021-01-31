a52662d62223119ddcca0d971bc22da52a14b247
Morgan McDonald <morganmcdee13@outlook.com>
2021-01-31 21:46:10 +0000

    Django-Heroku

--- HammondRealty/settings.py
+++ HammondRealty/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -149,3 +150,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
